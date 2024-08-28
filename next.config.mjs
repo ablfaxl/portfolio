0f19740a1ab341f2e0015b1d6920163db653dd0b
ablfaxl <taghavi.a2023@gmail.com>
2024-08-28 17:44:31 +0330

    chore: first commit 🚀

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
